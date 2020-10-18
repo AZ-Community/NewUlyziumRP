45ee066b08b523758c48e4f5f81b84cc4cd519c1
Vooxo <vooxo@outlook.fr>
2020-10-18 12:13:22 +0200

    Amélioration de l'inventaire

--- handler/Inventory.js
+++ handler/Inventory.js
@@ -1,8 +1,14 @@
 const fs = require('fs');
-module.exports = client => {
+module.exports = client => {	
+	var itemsTemp = require('../json/armes/armes-item.json');
 	const items = new Map();
-	items.set(0, "Test name")
-      /**
+	itemsTemp.forEach(item => {
+		items.set(item.id, item);
+	});
+	
+	client.itemsTemp = null;
+
+	/*
      * client.giveToPlayerItem -> Give Player item
      * @param {*} idPlayer - Player id.
      * @param {*} itemID - Item id.
@@ -10,7 +16,17 @@ module.exports = client => {
      */
     client.giveToPlayerItem = (idPlayer, itemID, quantity) => {
         return new Promise((resolve, reject) => {
-            client.con.query(`INSERT INTO inventory (idplayer, itemid, quantity) VALUES (${idPlayer},${itemID}, ${quantity})`);
+			client.con.query('SELECT * FROM inventory WHERE idplayer =' + idPlayer, (err, rows) => {
+				if(rows[0].id != itemID){
+					client.con.query(`INSERT INTO inventory (idplayer, itemid, quantity) VALUES ('${idPlayer}','${itemID}', '${quantity}')`, (err) => {
+						if(err) reject(err);
+					});
+				} else{
+					client.con.query(`UPDATE inventory SET quantity= ${rows.quantity + quantity} WHERE idplayer = ${idPlayer} AND itemid= ${itemID}`, (err) => {
+						if(err) reject(err);
+					});
+				}
+			});		
         });
     }
 	/*
@@ -22,12 +38,24 @@ module.exports = client => {
 		for(let[key, value] of items){
 			if(itemID == key){
 				if(nameOrID == "id"){
-					return true
+					return true;
 				}else if(nameOrID == "name"){
-					return value;
+					return value.name;
 				}
 			}
 		}
-		return false
+		return false;
 	}	
+	client.returnInventory = (idPlayer) => {
+		return new Promise((resolve, reject) => {
+			var listMap = new Map();
+        	client.con.query("SELECT * FROM inventory WHERE idplayer=" + idPlayer, (err, rows) => {
+            	if(err) reject(err);
+            	if(rows.length >= 1){
+	            	for(let i = 0; i < rows.length; i++) listMap.set(client.itemInformation(rows[i].itemid, "name"), rows[i].quantity);
+    	            resolve(listMap);					
+            	}else resolve('Inventaire vide...');
+  			});
+		});
+	}
 }
